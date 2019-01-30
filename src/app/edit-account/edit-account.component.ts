aae14ebdfa855431893f5b9a09717f2d9ba4ed4e
Reddy <sahithi.reddy@capgemini.com>
2019-01-30 16:43:37 -0500

    add table for account transactions on edit-account component

--- src/app/edit-account/edit-account.component.ts
+++ src/app/edit-account/edit-account.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Time } from '@angular/common';
+
+export interface AccountTrans {
+  id: number;
+  currencyFrom: string;
+  currencyTo: string;
+  rate: number;
+  dateOfTransaction: string;
+}
+
+const ELEMENT_DATA: AccountTrans[] = [
+  {id: 9, currencyFrom: 'YEN', currencyTo: "USD", rate: 5.75, dateOfTransaction: "2019-02-01 12:10:30"},
+  {id: 6, currencyFrom: 'EURO', currencyTo: "USD", rate: 0.50, dateOfTransaction: "2019-02-01 12:10:30"},
+  {id: 7, currencyFrom: 'YEN', currencyTo: "EURO", rate: 10.75, dateOfTransaction: "2019-02-05 12:14:36"},
+  {id: 5, currencyFrom: 'POUND', currencyTo: "USD", rate: 1.75, dateOfTransaction: "2019-02-07 10:16:36"}
+ 
+];
 
 @Component({
   selector: 'app-edit-account',
   templateUrl: './edit-account.component.html',
   styleUrls: ['./edit-account.component.css']
 })
-export class EditAccountComponent implements OnInit {
 
+export class EditAccountComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'currencyFrom', 'currencyTo', 'rate', 'dateOfTransaction'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
