ae11a374cbf418d9b9f9dada96c8e1efda60d9ee
Hanif Carroll <hanif.carroll@capgemini.com>
2019-02-01 11:38:54 -0500

    Add validation on exchange inputs

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -4,25 +4,39 @@ import { HttpService } from '../http.service';
 @Component({
   selector: 'app-exchange',
   templateUrl: './exchange.component.html',
-  styleUrls: ['./exchange.component.css']
+  styleUrls: ['./exchange.component.css'],
 })
 export class ExchangeComponent implements OnInit {
-
-  constructor(private httpService: HttpService) { }
-  from = 'usd'
-  to = 'euro'
-  amount
-
+  constructor(private httpService: HttpService) {}
+  from = 'usd';
+  to = 'euro';
+  amount;
+  currencies = [
+    { value: 'usd', text: 'Dollar ($)' },
+    { value: 'pound', text: 'Pound (£)' },
+    { value: 'euro', text: 'Euro (€)' },
+    { value: 'yen', text: 'Yen (¥)' },
+    { value: 'peso', text: 'Peso ($)' },
+  ];
+  availableCurrencies = [
+    { value: 'pound', text: 'Pound (£)' },
+    { value: 'euro', text: 'Euro (€)' },
+    { value: 'yen', text: 'Yen (¥)' },
+    { value: 'peso', text: 'Peso ($)' },
+  ];
 
   calculateResult(from, to, amount) {
-    from = from.toLowerCase()
-    to = to.toLowerCase()
-   return this.httpService.getCalculation(from, to, amount)
+    from = from.toLowerCase();
+    to = to.toLowerCase();
 
-    
+    return this.httpService.getCalculation(from, to, amount);
   }
 
-  ngOnInit() {
+  onSelect(value) {
+    this.availableCurrencies = this.currencies.filter(
+      currency => currency.value !== value,
+    );
   }
 
+  ngOnInit() {}
 }
